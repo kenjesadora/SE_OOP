b1241c44b6c18cfbe6d34a6176ed0564c05a8617
KeneyJie <112768937+kenjesadora@users.noreply.github.com>
2026-04-28 04:39:45 +0000

    sync changes

--- 1a. pet_variables.py
+++ 1a. pet_variables.py
@@ -1,11 +1,11 @@
 name = 'Bonnie'
 animal_category = 'Cat'
-age = 3
-vaccinated = True
-ccard = '3423 2326 7543 1234'
-billing_address = '17 Park Drive, The Shire 2695'
-owner_name = 'Alex Ngyuen'
-account_balance = 129.95
+age = 4
+vaccinated = False
+ccard = '3423 2326 7533 1234'
+billing_address = '17 Park Street, The Shire 2695'
+owner_name = 'Alex Jones'
+account_balance = 14.95 
 
 # ACTIVITIES:
 #Theere are many ways to complete these tasks. How will you do them?
--- python/hello-world.py
+++ python/hello-world.py
@@ -1,1 +1,4 @@
-print("Hello,World!")
+line1 = "kenje is the best at coding"
+line2 = "kenje is a bum"
+
+print(f{'line1,line2'} )
